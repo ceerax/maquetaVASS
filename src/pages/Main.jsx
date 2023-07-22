b4ebcdfad47a806cb67ae4bb4af194dc04cbce7d
ceerax <cesarmoreno.20@hotmial.com>
2023-07-21 23:43:38 -0500

    ajustes

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,7 +4,6 @@ import Image from "../components/imageCenter/image";
 import SubtitleBody from "../components/subtitleBody/subtitlebody";
 import LivelyCurtain from "../components/livelyCurtain/livelyCurtain";
 import Work from "../components/Work/work";
-import Footer from "../components/footer/footer";
 const Main = () => {
   return (
     <div>
